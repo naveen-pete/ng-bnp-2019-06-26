e4378b6fd1447b91fd58a0945ea7c771f407ba70
naveen-pete <naveen.shenoy.blr@gmail.com>
2019-06-25 17:25:49 +0530

    add typescript examples to the repo

---
+++ apps/ts-examples/data-types.ts
@@ -0,0 +1,26 @@
+// int a = 10;
+
+// var a: number = 10;
+
+// syntax
+// var <variable-name>: <data-type> = <value>;
+var a: number;
+a = 10;
+console.log('a =', a);
+
+a = 200;
+console.log('a =', a);
+
+// ES6 / ES2015 - EcmaScript - specs
+let b: string = 'Hello TypeScript programmer';
+console.log('b =', b);
+
+b = 'Hi';
+console.log('b =', b);
+
+const c: string = "Constant string, cannot be changed.";
+console.log('c =', c);
+
+c = "abc";
+console.log('c =', c);
+
